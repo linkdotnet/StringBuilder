30cc6a9cc1d6b432543a919ccaa8ba91f5f00454
Steven Giesel <stgiesel35@gmail.com>
2022-04-07 09:07:31 +0200

    Added ValueType append benchmark

--- tests/LinkDotNet.StringBuilder.Benchmarks/Program.cs
+++ tests/LinkDotNet.StringBuilder.Benchmarks/Program.cs
@@ -1,33 +1,4 @@
-﻿using System.Text;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
-using LinkDotNet.StringBuilder;
+﻿using BenchmarkDotNet.Running;
+using LinkDotNet.StringBuilder.Benchmarks;
 
-BenchmarkSwitcher.FromAssembly(typeof(Benchmarks).Assembly).Run();
-
-[MemoryDiagnoser]
-public class Benchmarks
-{
-    [Benchmark]
-    public string DotNetStringBuilder()
-    {
-        var builder = new StringBuilder();
-        builder.AppendLine("That is the first line of our benchmark.");
-        builder.AppendLine("We can multiple stuff in here if want.");
-        builder.AppendLine("The idea is that we can resize the internal structure from time to time.");
-        builder.AppendLine("We can also add other Append method if we want. But we keep it easy for now.");
-        return builder.ToString();
-    }
-
-    [Benchmark]
-    public string ValueStringBuilder()
-    {
-        var builder = new ValueStringBuilder();
-        builder.AppendLine("That is the first line of our benchmark.");
-        builder.AppendLine("We can multiple stuff in here if want.");
-        builder.AppendLine("We can multiple stuff in here if want.");
-        builder.AppendLine("The idea is that we can resize the internal structure from time to time.");
-        builder.AppendLine("We can also add other Append method if we want. But we keep it easy for now.");
-        return builder.ToString();
-    }
-}
+BenchmarkSwitcher.FromAssembly(typeof(AppendBenchmarks).Assembly).Run();
