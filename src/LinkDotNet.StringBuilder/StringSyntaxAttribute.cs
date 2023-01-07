56e5dfacff2529aadb80f8dd9857b5f3a401b634
Steven Giesel <stgiesel35@gmail.com>
2023-01-07 11:35:16 +0100

    Copy&pasted documentation is sometimes okay

--- src/LinkDotNet.StringBuilder/StringSyntaxAttribute.cs
+++ src/LinkDotNet.StringBuilder/StringSyntaxAttribute.cs
@@ -1,7 +1,7 @@
 #if NET6_0
 namespace System.Diagnostics.CodeAnalysis;
 
-/// <summary>Fake version of the StringSyntaxAttribute, which was introduced in .NET 7</summary>
+/// <summary>Fake version of the StringSyntaxAttribute, which was introduced in .NET 7.</summary>
 [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Field | AttributeTargets.Property)]
 [SuppressMessage("Design", "CA1019:Define accessors for attribute arguments", Justification = "The sole purpose is to have the same public surface as the class in .NET7 and above.")]
 internal sealed class StringSyntaxAttribute : Attribute
