7331bfc4724bca50d3cfc88f836d6e850ded3c89
Steven Giesel <stgiesel35@gmail.com>
2024-06-09 20:51:32 +0200

    Readded Benchmark

--- tests/LinkDotNet.StringBuilder.Benchmarks/AppendBenchmark.cs
+++ tests/LinkDotNet.StringBuilder.Benchmarks/AppendBenchmark.cs
@@ -5,6 +5,17 @@ namespace LinkDotNet.StringBuilder.Benchmarks;
 [MemoryDiagnoser]
 public class AppendBenchmarks
 {
+    [Benchmark(Baseline = true)]
+    public string DotNetStringBuilder()
+    {
+        var builder = new System.Text.StringBuilder();
+        builder.AppendLine("That is the first line of our benchmark.");
+        builder.AppendLine("We can multiple stuff in here if want.");
+        builder.AppendLine("The idea is that we can resize the internal structure from time to time.");
+        builder.AppendLine("We can also add other Append method if we want. But we keep it easy for now.");
+        return builder.ToString();
+    }
+
     [Benchmark]
     public string ValueStringBuilder()
     {
